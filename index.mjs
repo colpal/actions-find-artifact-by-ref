0f32fd19ffffe635d0f42701c92bda9b1f94813c
Nicholas Farley <nicholasafarley@gmail.com>
2022-02-14 16:10:51 -0500

    Pull check suites by ref

--- index.mjs
+++ index.mjs
@@ -1,1 +1,23 @@
-console.log('Hello, World!');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function main() {
+  const token = core.getInput('github-token', { required: true });
+  const octokit = github.getOctokit(token);
+  const ref = core.getInput('ref', { required: true });
+  const check_name = core.getInput('check-name');
+
+  const check_suites = await octokit.paginate(
+    octokit.rest.checks.listSuitesForRef,
+    {
+      ...github.context.repo,
+      ref,
+      check_name,
+      status: 'completed',
+    },
+  );
+
+  console.log(check_suites);
+}
+
+main();
