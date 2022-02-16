a2e0095e8e8ba7f6fd50d680e05322a40a5c56d5
Nicholas Farley <nicholasafarley@gmail.com>
2022-02-16 13:44:37 -0500

    Update all inputs/outputs to use underscores rather than hyphens

--- index.mjs
+++ index.mjs
@@ -2,10 +2,10 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 async function main() {
-  const token = core.getInput('github-token', { required: true });
+  const token = core.getInput('github_token', { required: true });
   const octokit = github.getOctokit(token);
   const ref = core.getInput('ref', { required: true });
-  const check_name = core.getInput('check-name');
+  const check_name = core.getInput('check_name');
 
   const check_suites = await octokit.paginate(
     octokit.rest.checks.listSuitesForRef,
